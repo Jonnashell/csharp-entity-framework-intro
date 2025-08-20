7d191d1d633f5c425fd18cd7675806bfa575d7d7
Jonatan Berg Romundgard <jonatan.romundgard@no.experis.com>
2025-08-20 10:52:45 +0200

    Added a JsonIgnore for a DTO when author is null

--- exercise.webapi/DTOs/BookExtGet.cs
+++ exercise.webapi/DTOs/BookExtGet.cs
@@ -1,9 +1,12 @@
-﻿namespace exercise.webapi.DTOs
+﻿using System.Text.Json.Serialization;
+
+namespace exercise.webapi.DTOs
 {
     public class BookExtGet
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public AuthorExtGet Author { get; set; }
     }
 }
